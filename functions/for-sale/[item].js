231bd5786a198ad7a78d762d785869150c537678
volkanger <volkanger@gmail.com>
2023-04-06 01:31:09 -0400

    updates to shape it like a real search engine

--- functions/for-sale/[item].js
+++ functions/for-sale/[item].js
@@ -12,49 +12,42 @@ export async function onRequest({ context, env, request  }) {
     //learn how to patch pages to kv
     // const allKeys = await env.marketplace.get("available:T01C79S2HKP:U01JP332EP7:1616534585127")
     const allKeys = (await env.marketplace.list({"prefix": "available:"})).keys
-    console.log(allKeys)
-    console.log(allKeys)
-    return new Response(JSON.stringify(allKeys))
-    //     if (allKeys.length == 0) { //no listings ever.
-    //         return new JsonResponse({
-    //           type: 4,
-    //           data: {
-    //             content: `Looks like a fresh install! Use /sell to add the first item.`,
-    //             flags: 64
-    //           },
-    //         });
-    //       } else {
-    //         async function search(keyword) {
-    //             var foundKeys = 0
-    //             var i = 0
-    //             var searchResults = ""
-    //             for (i = 0; i < allKeys.length; i++) {
-    //                 console.log(i)
-    //               const values = await context.env.marketplace.get(allKeys[i].name)
-    //               console.log("values: " + values)
-    //               if (values && values.toLowerCase().includes(item.toLowerCase())) {
-    //                 foundKeys = foundKeys + 1
-    //                 console.log("matching values: " + values)
-    //                 //split the key so we know the seller team and userID
-    //                 const item_owner_user_id = allKeys[i].name.split(":")[3]
-    //                 //allKeys[i] is something like: "available:discord:T151XMC12:U1VALTVUY:1610241853179"
-    //                 //split 0 is available
-    //                 //split1 is discord
-    //                 //split 2 is team, split 3 is user
-    //                 //use sections[1] + sections[2] down instead of team_id and user_id below
-    //                 searchResults = `${searchResults} \n ${values} from <@${item_owner_user_id}>`
-    //                 console.log(searchResults)
-    //               } 
-    //             }
-    //             console.log(searchResults)
-    //             if (searchResults.length == 0) {
-    //                 console.log("nothing found")
+
     
-    //             }
-    //         }
-    
-    //     return searchResults
-    //         }
+        if (allKeys.length == 0) { //no listings ever.
+            return new Response("No dice")
+        } else {
+          async function search(keyword) {
+              var foundKeys = 0
+              var i = 0
+              var searchResults = ""
+              for (i = 0; i < allKeys.length; i++) {
+                  console.log(i)
+                const values = await env.marketplace.get(allKeys[i].name)
+                console.log("values: " + values)
+                if (values && values.toLowerCase().includes(item.toLowerCase())) {
+                  foundKeys = foundKeys + 1
+                  console.log("matching values: " + values)
+                  //split the key so we know the seller team and userID
+                  const item_owner_user_id = allKeys[i].name.split(":")[3]
+                  //allKeys[i] is something like: "available:discord:T151XMC12:U1VALTVUY:1610241853179"
+                  //split 0 is available
+                  //split1 is discord
+                  //split 2 is team, split 3 is user
+                  //use sections[1] + sections[2] down instead of team_id and user_id below
+                  searchResults = `${searchResults} \n ${values} from <@${item_owner_user_id}>`
+                  console.log(searchResults)
+                } 
+              }
+              console.log(searchResults)
+              if (searchResults.length == 0) {
+                  console.log("nothing found")
+  
+              }
+          }
+  
+            return new Response(searchResults)
+        }
 
 
 
