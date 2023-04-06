2c1faa2ce072290bad0f32804d0d335acfd781be
volkanger <volkanger@gmail.com>
2023-04-05 23:21:51 -0400

    folder structure test

---
+++ functions/for-sale/index.js
@@ -0,0 +1,9 @@
+export default function forSalePage() {
+    return (
+        <main>
+            <h1> All listings on /for-sale </h1>
+            <h2>/for-sale/iphone for search results</h2>
+            <h3>/for-sale/$id for details page</h3>
+        </main>
+    )
+}
