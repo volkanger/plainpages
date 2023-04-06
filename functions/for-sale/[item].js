feb9cb998fd7d2bb15716914bcf38850c788a1bf
volkanger <volkanger@gmail.com>
2023-04-06 15:14:13 -0400

    head

--- functions/for-sale/[item].js
+++ functions/for-sale/[item].js
@@ -50,7 +50,23 @@ export async function onRequest({ context, env, request  }) {
     console.log(JSON.stringify(searcheResults))
 
     let html = `
-    <html>
+    <!doctype html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="author" content="ThemeStarz">
+    
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Varela+Round" rel="stylesheet">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="assets/fonts/font-awesome.css" type="text/css">
+        <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/user.css">
+    
+      <title>Craigs - Easy Buy & Sell Listing HTML Template</title>
+    
+    </head>
     <body>
     <div class="page home-page">
         <!--*********************************************************************************************************-->
@@ -1446,9 +1462,7 @@ export async function onRequest({ context, env, request  }) {
 
 	<script src="assets/js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="assets/js/popper.min.js"></script>
-	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyBEDfNcQRmKQEyulDN8nGWjLYPm8s4YB58&libraries=places"></script>
-    <!--<script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>-->
+	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script> 
 	<script src="assets/js/selectize.min.js"></script>
 	<script src="assets/js/masonry.pkgd.min.js"></script>
 	<script src="assets/js/icheck.min.js"></script>
