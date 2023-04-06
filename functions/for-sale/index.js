b38a19b73fd8e2709b960888a92ce35d478e307b
volkanger <volkanger@gmail.com>
2023-04-06 00:08:48 -0400

    pages are getting stacked

--- functions/for-sale/index.js
+++ functions/for-sale/index.js
@@ -6,6 +6,9 @@ export async function onRequest(request) {
             <h1> All listings on /for-sale </h1>
             <h2>/for-sale/iphone for search results</h2>
             <h3>/for-sale/$id for details page</h3>
+            
+            <h3>So, from this page, let's send a post to /for-sale/keyword-with-hyphens</h3>
+            <h3>And update the [item].js file to be the search result page, using the posted data as keyword</h3>
 
 		</body>`;
 
