1163827cdb8cf49c073e42da36ccf42cb1642c13
volkanger <volkanger@gmail.com>
2023-04-18 00:00:43 -0400

    final

--- functions/update/index.js
+++ functions/update/index.js
@@ -1,57 +1,57 @@
 export async function onRequest({ context, env, request  }) {
 
- let availables = JSON.parse(await env.marketplace.get("availables"))
- console.log(availables)
- console.log(JSON.stringify(availables))
- let newest = {
-  "key": "available:T:U:Timey", 
-  "value": "satılık iphoney"
-  }
-  console.log(newest)
-  availables.push(newest)
-  console.log(availables)
+//  let availables = JSON.parse(await env.marketplace.get("availables"))
+//  console.log(availables)
+//  console.log(JSON.stringify(availables))
+//  let newest = {
+//   "key": "available:T:U:Timey", 
+//   "value": "satılık iphoney"
+//   }
+//   console.log(newest)
+//   availables.push(newest)
+//   console.log(availables)
+
+//  return new Response(JSON.stringify(availables))
 
 
- return new Response(JSON.stringify(newest))
+    const allKeys = (await env.marketplace.list({"prefix": "available:T151XMC12:"})).keys
+    // //all discord
+    // const availablesDiscord = await env.marketplace.get("availables:discord")
+    // //all slack
+    // const availablesSlack = await env.marketplace.get("availables:slack")
+    let availables = await env.marketplace.get("availables") //array
 
-    // const allKeys = (await env.marketplace.list({"prefix": "available:T151XMC12:"})).keys
-    // // //all discord
-    // // const availablesDiscord = await env.marketplace.get("availables:discord")
-    // // //all slack
-    // // const availablesSlack = await env.marketplace.get("availables:slack")
-    // let availables = await env.marketplace.get("availables") //array
-
-    // for (let i = 0; i < allKeys.length; i++) {
-    //     const value = await env.marketplace.get(allKeys[i].name)
-    //     console.log("Current Value:")
-    //     console.log(value)
-    //     availables = JSON.parse(availables)
-    //     console.log("Availables:")
-    //     console.log(availables)
-    //     //push the object
-    //     // console.log("Availables type:")
-    //     // console.log(typeof availables)
-    //     // let availablesArray = JSON.parse(availables)
-    //     // console.log("AvailablesArray type:")
-    //     // console.log(typeof availablesArray)
-    //     let newPair = {"key": allKeys[i].name, "value": value}
-    //     console.log("newPair:")
-    //     console.log(newPair)
-    //     availables.push(newPair)
-    //     console.log("availables after push:")
-    //     console.log(availables)
-    //     if (i == allKeys.length-1) { //last one. Not sure if I can get the value out of here.
-    //       console.log(availables)
-    //       await env.marketplace.put(availables, JSON.stringify(availables))
-    //     }
-    // }
+    for (let i = 0; i < allKeys.length; i++) {
+        const value = await env.marketplace.get(allKeys[i].name)
+        console.log("Current Value:")
+        console.log(value)
+        availables = JSON.parse(availables)
+        console.log("Availables:")
+        console.log(availables)
+        //push the object
+        // console.log("Availables type:")
+        // console.log(typeof availables)
+        // let availablesArray = JSON.parse(availables)
+        // console.log("AvailablesArray type:")
+        // console.log(typeof availablesArray)
+        let newPair = {"key": allKeys[i].name, "value": value}
+        console.log("newPair:")
+        console.log(newPair)
+        availables.push(newPair)
+        console.log("availables after push:")
+        console.log(availables)
+        if (i == allKeys.length-1) { //last one. Not sure if I can get the value out of here.
+          console.log(availables)
+          await env.marketplace.put("availables", availables)
+        }
+    }
     
-    // console.log(availables)
+    console.log(availables)
 
-    // return new Response("Thank you for the new values. Let's see if it worked. check the logs. Hope you were tailing 'em.", {
-    //     headers: {
-    //       "content-type": "text/plain;charset=UTF-8",
-    //     },
-    //   });
+    return new Response("Thank you for the new values. Let's see if it worked. check the logs. Hope you were tailing 'em.", {
+        headers: {
+          "content-type": "text/plain;charset=UTF-8",
+        },
+      });
 
 }
