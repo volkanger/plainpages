745ee0689d14356aa9ffd7aaff5fb82f5b2028d5
volkanger <volkanger@gmail.com>
2023-04-23 20:40:18 -0400

    publish por favor

--- functions/for-sale/[item].js
+++ functions/for-sale/[item].js
@@ -29,7 +29,7 @@ export async function onRequest({ context, env, request  }) {
         if (item.title.includes(keyword)) {
           console.log(item)
           console.log("found one: " + item.title)
-					itemsFound[key] = item
+					itemsFound[key] = JSON.stringify(item)
         }
       })
       // console.log(itemsFound)
